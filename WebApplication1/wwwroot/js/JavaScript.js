ec33066948ce03654fb8ee21988e7c33b2a3134e
William Gecawich <bgecawich@smartmobilestrategies.com>
2021-06-07 12:06:27 -0400

    Add project files.

---
+++ WebApplication1/wwwroot/js/JavaScript.js
@@ -0,0 +1,28 @@
+﻿window.jsfunction =
+{
+    focuselement: function (id) {
+        const element = document.getElementById(id);
+        if (!element)
+            return;
+        element.focus();
+    },
+
+    selectelement: function (id) {
+        const element = document.getElementById(id);
+        if (element) {
+            element.select();
+        }
+    },
+    blurelement: function (id) {
+        const element = document.getElementById(id);
+        if (element) {
+            element.blur();
+        }
+    },
+    getFocusedElement: function () {
+        return document.activeElement.id;
+    },
+    SetPageTitle: function (title) {
+        document.title = title;
+    }
+}
